34d157db535ecb5e4c50ebeecf2933e5e5612685
oibafs <fabioscaravelli@gmail.com>
2021-04-01 17:45:47 -0300

    API key to client.js

--- public/js/client.js
+++ public/js/client.js
@@ -512,7 +512,7 @@ TrelloPowerUp.initialize({
     
     // If we want to ask the user to authorize our Power-Up to make full use of the Trello API
     // you'll need to add your API from trello.com/app-key below:
-    let trelloAPIKey = '';
+    let trelloAPIKey = process.env.TRELLOKEY;
     // This key will be used to generate a token that you can pass along with the API key to Trello's
     // RESTful API. Using the key/token pair, you can make requests on behalf of the authorized user.
     
